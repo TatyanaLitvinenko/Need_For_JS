7c82d4189432a64b2651b45ea7eeac9bc90ebdc0
Litvinenko Tatyana <dead_jane@icloud.com>
2020-09-11 15:48:00 +0500

    lesson3

--- main.js
+++ main.js
@@ -28,7 +28,8 @@ function getQuantityElements(heightElement) {
 
 function startGame() {
     start.classList.add('hide');
-
+    gameArea.innerHTML = '';
+    
     for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
@@ -45,10 +46,14 @@ function startGame() {
         enemy.style.top = enemy.y + 'px';
         enemy.style.background = 'transparent url("./image/enemy3.png") center / cover no-repeat';
         gameArea.appendChild(enemy);
-    }
+    };
 
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
@@ -57,35 +62,37 @@ function startGame() {
 function playGame() {
     
     if (setting.start) {
+        setting.score += setting.speed;
+        score.textContent = 'SCORE ' + setting.score;
         moveRoad();
         moveEnemy();
         if(keys.ArrowLeft && setting.x > 0) {
             setting.x -= setting.speed;
-        }
+        };
         if(keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
             setting.x += setting.speed;
-        }
+        };
 
         if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
             setting.y += setting.speed;
-        }
+        };
 
         if (keys.ArrowUp && setting.y > 0) {
             setting.y -= setting.speed;
-        }
+        };
 
         car.style.left = setting.x + 'px';
         car.style.top = setting.y + 'px';
 
         requestAnimationFrame(playGame);
-    }
+    };
 }
 
 function startRun(evt) {
     if(evt.key !== 'F5' && evt.key !== 'F12') {
         evt.preventDefault();
     keys[evt.key] = true;
-    }
+    };
     
 }
 
@@ -102,13 +109,26 @@ function moveRoad() {
 
         if (line.y >= document.documentElement.clientHeight) {
             line.y = -100;
-        }
-    })
+        };
+    });
 }
 
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function(item) {
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+        if (carRect.top <= enemyRect.bottom &&
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+                setting.start = false;
+                console.warn('ДТП');
+                start.classList.remove('hide');
+                start.style.top = score.offsetHeight;
+        };
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if (item.y >= document.documentElement.clientHeight) {
